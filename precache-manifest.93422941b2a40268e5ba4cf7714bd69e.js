53b5e6839158411b21847b6f9e381a50df8b05bc
pabloc33 <68171460+pabloc33@users.noreply.github.com>
2020-09-28 15:19:12 -0300

    Updates

---
+++ precache-manifest.93422941b2a40268e5ba4cf7714bd69e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0091917119bd23b37a4a8bbb4b6364d",
+    "url": "/08-journal-app/index.html"
+  },
+  {
+    "revision": "e0ffe6add9f68f3cba2e",
+    "url": "/08-journal-app/static/css/main.67357bb9.chunk.css"
+  },
+  {
+    "revision": "ed8dd25b3622347b582f",
+    "url": "/08-journal-app/static/js/2.385e2a0b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/08-journal-app/static/js/2.385e2a0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0ffe6add9f68f3cba2e",
+    "url": "/08-journal-app/static/js/main.0f71adbb.chunk.js"
+  },
+  {
+    "revision": "e0f651120c19713f651e",
+    "url": "/08-journal-app/static/js/runtime-main.8120ddf3.js"
+  }
+]);
